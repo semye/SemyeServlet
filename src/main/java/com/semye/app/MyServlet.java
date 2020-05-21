013fce8e9ad531e56b76fd050de009cb5cf6f146
semye <464622832@qq.com>
2020-05-21 18:48:20 +0800

    commit

---
+++ src/main/java/com/semye/app/MyServlet.java
@@ -0,0 +1,23 @@
+package com.semye.app;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+        System.out.println("=====>" + Thread.currentThread().getName());
+        printWriter.println("{\"a\":\"b\"}");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: yesheng
+  Date: 2019-06-06
+  Time: 15:25
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>$Title$</title>
+</head>
+<body>
+hello world!!!
+</body>
+</html>
